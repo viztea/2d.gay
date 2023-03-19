0ee17794b2820d6874c7e192f39828ff0223c763
melike2d <real.melike2d@gmail.com>
2023-03-19 09:01:23 -0400

    chore: cloudflare adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,11 @@
-import adapterAuto from "@sveltejs/adapter-auto";
-import adapterCloudflare from "@sveltejs/adapter-cloudflare";
+import cloudflare from "@sveltejs/adapter-cloudflare";
 import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: process.env.NODE_ENV === "production"
-            ? adapterCloudflare()
-            : adapterAuto()
+		adapter: cloudflare()
 	},
     
 };
